4bf1ffb6b386b32bb3c8465f1df0f91325da3acc
Daniel Fernandez <dtfern@gmail.com>
2017-06-05 23:47:52 +0100

    Start of fake input creation

---
+++ BrandSheet/Helpers/FakeInputHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrandSheet.Helpers {
+    public static class FakeInputHelper {
+        public static IHtmlString Radio(this MvcHtmlString helper, string text) {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<label class=\"fakeinput radio\">");
+            //builder.Append("<input type=\"radio\" name=\"" + name + "\" " + (isChecked ? "checked=\"checked\"" : "") + ">");
+            builder.Append(helper.ToString());
+            builder.Append("<div class=\"fakebox\"><div class=\"fakefill\"></div></div>");
+            builder.Append("<span>" + text + "</span>");
+            builder.Append("</label>");
+            return new HtmlString(builder.ToString());
+        }
+    }
+}
