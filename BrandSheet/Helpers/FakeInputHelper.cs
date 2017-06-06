ea2f2bfe126dc427ed4dc4f17fcdc2544086276f
Daniel Fernandez <dtfern@gmail.com>
2017-06-06 22:44:02 +0100

    Radio styled and Checkbox created

--- BrandSheet/Helpers/FakeInputHelper.cs
+++ BrandSheet/Helpers/FakeInputHelper.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Mvc.Html;
 namespace BrandSheet.Helpers {
     public static class FakeInputHelper {
-        public static IHtmlString Radio(this MvcHtmlString helper, string text) {
+        public static IHtmlString Radio(string name, bool isChecked, string value, string text) {
             StringBuilder builder = new StringBuilder();
             builder.Append("<label class=\"fakeinput radio\">");
-            //builder.Append("<input type=\"radio\" name=\"" + name + "\" " + (isChecked ? "checked=\"checked\"" : "") + ">");
-            builder.Append(helper.ToString());
+            builder.Append("<input type=\"radio\" value=\""+ value + "\" name=\"" + name + "\" " + (isChecked ? "checked=\"checked\"" : "") + " />");
+            builder.Append("<div class=\"fakebox\"><div class=\"fakefill\"></div></div>");
+            builder.Append("<span>" + text + "</span>");
+            builder.Append("</label>");
+            return new HtmlString(builder.ToString());
+        }
+
+        public static IHtmlString Checkbox(string name, bool isChecked, string text) {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<label class=\"fakeinput checkbox\">");
+            builder.Append("<input type=\"checkbox\" name=\"" + name + "\" " + (isChecked ? "checked" : "") + " />");
             builder.Append("<div class=\"fakebox\"><div class=\"fakefill\"></div></div>");
             builder.Append("<span>" + text + "</span>");
             builder.Append("</label>");
